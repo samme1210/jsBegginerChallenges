08b819a4ad75bb613b34d26d89d13d6786b17b58
samme1210 <samme1210@gmail.com>
2022-10-16 23:46:29 +0100

    begginer complete

---
+++ app.js
@@ -0,0 +1,53 @@
+/*
+    Q1 - hours to seconds 
+
+function hoursIntoSeconds(hour) {
+    return 60 * (hour * 60);
+}
+
+console.log(hoursIntoSeconds(2));
+*/
+
+/* 
+    Q2 - calc perimeter of rectangle
+
+
+function perimeterRectangle(length, width) {
+    return length * 2 + width * 2;
+}
+
+console.log(perimeterRectangle(6, 7));
+*/
+
+/*
+    Q3 - extend a string 
+
+
+function appendFrontend(string) {
+    return string + "frontend"
+}
+
+console.log(appendFrontend("apple"));
+*/
+
+/*
+    Q4 - is the number odd
+
+
+function isNumOdd(number) {
+    return number % 2 === 0?  1 : -1; 
+}
+
+console.log(isNumOdd(6))
+*/
+
+/*
+    Q5 - logged in and subscribed
+
+
+function isloggedAndSubbed(strOne, strTwo) {
+    return strOne === "Logged_in" && strTwo === "Subscribed"? true : false; 
+}
+
+console.log(isloggedAndSubbed("Logged_in", "Subscribed"));
+*/
